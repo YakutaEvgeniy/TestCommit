a301132ede13dee635e80229f3c77b4982f3518d
YakutaEvgeniy <yakutawork@gmail.com>
2021-06-29 16:43:12 +0300

    Test

---
+++ SeleniumZero/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            string text = driver.FindElement(By.ClassName("RNmpXc")).Text;
+            Console.WriteLine("Hello eghwH"+text);
+            driver.Quit();
+        }
+    }
+}
